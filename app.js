813e03c3c1a9c5e652e3e1a13e70486d4b200306
BixBee96 <jammerking96@gmail.com>
2020-09-24 18:24:51 +0530

    added html css and js code

---
+++ app.js
@@ -0,0 +1,12 @@
+const choises = {
+    'rock' : document.getElementById("rock"),
+    'paper' : document.getElementById("paper"),
+    'scissors': document.getElementById("scissors")
+}
+
+console.log(choises,);
+
+
+// function playRPS () {
+//     let playerChoise 
+// }
--- main.js
+++
@@ -1,4 +0,0 @@
-let playerScore = 0;
-let computerScore = 0;
-
-function scoreCount()
